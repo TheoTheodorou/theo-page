a523dff66aa309a9832d4fc788b222684928773f
Theo Theodorou <theodore.theodorou@mac.com>
2022-11-30 14:06:29 +0000

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Navbar from './Components/NavBar/Navbar';
 import Home from './Modules/Home/Home';
 import Work from './Modules/Work/Work';
 import About from './Modules/About/About';
-import NavbarTest from './Components/NavBar/NavbarTest';
 
 function App() {
   return (
     <div className="App">
-      <NavbarTest />
+      <Navbar />
 
       <Routes>
         <Route exact path="/" element={<Home />} />
